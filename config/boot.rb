df8a1101057f22b474c0db45e5f91eb9f9dd0c48
Terugu-Kiran <kiran.terugu@gmail.com>
2020-12-31 09:46:57 +0530

    A

--- config/boot.rb
+++ config/boot.rb
@@ -1,9 +1,4 @@
-# ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
-# require "bundler/setup" # Set up gems listed in the Gemfile.
-# # require "bootsnap/setup" # Speed up boot time by caching expensive operations.
-
-
-ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
-
-require 'bundler/setup' # Set up gems listed in the Gemfile.
+require "bundler/setup" # Set up gems listed in the Gemfile.
+require "bootsnap/setup" # Speed up boot time by caching expensive operations.
