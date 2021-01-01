89e254bff90fc247606fd89ab06d4159e99fee78
kiran1167 <kiran61167@gmail.com>
2021-01-01 23:49:21 +0530

    a

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module Jobs
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    config.time_zone = 'Kolkata'
+
+    config.assets.initialize_on_precompile = false
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
